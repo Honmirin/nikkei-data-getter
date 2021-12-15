39ee13dc083b08b26e4e949eef8ee9ed16a08b82
Honmirin <kodama.ruby.note@gmail.com>
2021-12-15 23:34:28 +0900

    delete financialmode

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,30 +1,7 @@
 package com.example;
 
-import java.util.ArrayList;
-
 public class Main {
   public static void main(String[] args) {
-    System.out.println("こんにちは、モードを選んでください");
-    System.out.println("1.企業一覧作成 2.財政情報表示");
-    int mode = new java.util.Scanner(System.in).nextInt();
-
-    switch (mode) {
-      case 1:
-        serchCompany();        
-        break;
-      case 2:
-        getFinancialData();
-        break;
-      default:
-        System.out.println("1または2を入力してください");
-        break;
-    }
-  }
-  public static void getFinancialData() {
-    ArrayList<Integer> companyNumbers = receivingNumbers();
-    
-  }
-  public static void serchCompany() {
     SerchCompanyPage page = new SerchCompanyPage();
     try {
       page.loadPage();
@@ -55,25 +32,5 @@ public class Main {
         break;
     }
     System.out.println("-------------------");
-  }
-  private static ArrayList<Integer> receivingNumbers() {
-    ArrayList<Integer> returnNumbers = new ArrayList<>();
-
-    System.out.println("数字を入力してください、終了する場合は「end」と入力してください");
-    while (true) {
-      System.out.print(">");
-      String word = new java.util.Scanner(System.in).nextLine();
-      if (word == "end") {
-        break;
-      } else {
-        try {
-          int number = Integer.parseInt(word);
-          returnNumbers.add(number);
-        } catch (NumberFormatException e) {
-          System.out.println("数字またはendを入力してください");
-        }
-      }
-    }
-    return returnNumbers;
   }
 }
